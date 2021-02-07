68c184e7325dd38c79adf6e7d5f533e3aaf7ec8b
赖伟东 <488802592@qq.com>
2021-02-07 23:03:43 +0800

    v1提交

--- p2p/src/main/java/com/takemehand/p2p/P2PApplication.java
+++ p2p/src/main/java/com/takemehand/p2p/P2PApplication.java
@@ -8,6 +8,7 @@ public class P2PApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P2PApplication.class, args);
+		Server.init();
 		System.out.println("系统启动正常");
 	}
 
